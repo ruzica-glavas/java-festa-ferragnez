5540ba5b0202f68c9eb0b2c9c35fe35b83e4c8fa
ruzica-glavas <ruzi99@hotmail.it>
2025-04-30 18:32:58 +0200

    first commit

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,7 @@
+package com.ferragnez.party;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis, Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+    }
+}
