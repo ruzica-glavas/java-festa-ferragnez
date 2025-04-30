15572ca08d53fd75250d36c1fb20fa1dc84b16ae
ruzica-glavas <ruzi99@hotmail.it>
2025-04-30 19:10:22 +0200

    add: circle for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,34 @@
 package com.ferragnez.party;
+import java.util.Scanner;
 
 public class CheckGuest {
     public static void main(String[] args) {
         String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis, Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Come ti chiami?");
+        String nome = input.nextLine();
+
+        boolean foundName = false;
+
+        for(int i=0; i<guests.length; i++){
+            //System.out.println(guests[i]);
+
+            if (nome.equals (guests[i])){
+                System.out.println("Il tuo nome fa parte della lista, quindi puoi entrare");
+                foundName=true;
+                break;
+               
+            } 
+        }
+        
+        if (!foundName){
+            System.out.println("Il tuo nome non fa parte della lista quindi non puoi entrare");
+        }
+        
+
+
+        input.close();
     }
 }
